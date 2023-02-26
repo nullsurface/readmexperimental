943f518342b84b5304e0e0420ac9c1495a27808f
Abdulqadir_Abuharrus <aabuharrus@gmail.com>
2023-02-25 17:03:25 -0800

    Node.JS setup

--- app.js
+++ app.js
@@ -13,3 +13,17 @@ app.use(express.urlencoded({
 }));
 
 app.use(express.static("public"));
+
+
+
+app.get("/", function(req, res){
+  res.render("index");
+});
+
+let port = process.env.PORT;
+if(port == null || port == ""){
+  port = 3000;
+}
+app.listen(port, function() {
+  console.log("Server started successfully.");
+});
